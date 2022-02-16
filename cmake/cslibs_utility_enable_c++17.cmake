2abb085513e79e6d1e044380f35672ff823a939a
Richard Schulz <richard.schulz@daimler.com>
2022-02-16 12:52:24 +0100

    new cmake c++17 macro

--- cmake/cslibs_utility_enable_c++17.cmake
+++ cmake/cslibs_utility_enable_c++17.cmake
@@ -1,10 +1,3 @@
 ## Enforce that we use C++17
-include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-std=c++17" COMPILER_SUPPORTS_CXX17)
-if(COMPILER_SUPPORTS_CXX17)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
-else()
-    message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++11 support. Please use a different C++ compiler.")
-endif()
-unset(COMPILER_SUPPORTS_CXX17 CACHE)
-
+cmake_minimum_required(VERSION 3.1 FATAL_ERROR)
+set(CMAKE_CXX_STANDARD 17)
